783e5c2871498290b6d6154093ca6bd9d54a8e66
Elayamani Krishnamoorthi <k.elayamani@gmail.com>
2021-10-17 13:26:15 +0530

    cors bypass nodejs app

--- README.md
+++ README.md
@@ -5,15 +5,16 @@ CORS ByPass URL: https://us-central1-mani-coder.cloudfunctions.net/cors-bypass/
 Just append the original URL to end of the CORS bypass URL and you are good to go. However there is a catch to this, you need to replace the double slash after http or https (http://) with single slash to make it work.
 
 For example:
-Original URL: https://portfolio.nasdaq.com/api/portfolio/getPortfolioEvents/?fromDate=2021-10-16&toDate=2022-02-16&tickers=BIDU
+Original URL: https://example.com
 
 To fetch data using cors-bypass, you need to use the URL like below:
+
 ```
-https://us-central1-mani-coder.cloudfunctions.net/cors-bypass/https:/portfolio.nasdaq.com/api/portfolio/getPortfolioEvents/?fromDate=2021-10-16&toDate=2022-02-16&tickers=BIDU
+https://us-central1-mani-coder.cloudfunctions.net/cors-bypass/https:/example.com
 ```
 
 Simply you have to do this in your code:
-https://us-central1-mani-coder.cloudfunctions.net/cors-bypass/<url_to_fetch_data.replace('//', '/')>
+https://us-central1-mani-coder.cloudfunctions.net/cors-bypass/<your_url.replace('//', '/')>
 
 ```
 export function buildCorsFreeUrl(target: string): string {
@@ -23,7 +24,7 @@ export function buildCorsFreeUrl(target: string): string {
 fetch(
    buildCorsFreeUrl("https://api-to-fetch/")
 ).then(
-   ...  
+   ...
 ).catch(
    ...
 )
---
+++ nodejs/src/index.ts
@@ -0,0 +1,54 @@
+import * as bodyParser from "body-parser";
+import express from "express";
+import request from "request";
+import URL from "url";
+
+const app = express();
+
+const limit = typeof process.argv[2] != "undefined" ? process.argv[2] : "100kb";
+console.log("Using limit: ", limit);
+
+app.use(bodyParser.json({ limit: limit }));
+
+app.all("*", function (req, res) {
+  // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    req.header("access-control-request-headers")
+  );
+  res.header("Cache-Control", "public, max-age=3600");
+
+  // skip favicon.ico.
+  if (req.path === "/favicon.ico") {
+    res.send();
+    return;
+  }
+
+  if (req.method === "OPTIONS") {
+    // CORS Preflight
+    res.send();
+  } else {
+    const parsedUrl = URL.parse(req.url);
+    const targetURL = parsedUrl.path?.slice(1);
+    if (!targetURL) {
+      res.status(500);
+      res.send({ error: "Append your url to the end of the cors-bypass URL." });
+      return;
+    }
+
+    request({
+      url: targetURL + req.url,
+      method: req.method,
+      json: req.body,
+      headers: { Authorization: req.header("Authorization") },
+    }).pipe(res);
+  }
+});
+
+app.set("port", process.env.PORT || 3000);
+
+app.listen(app.get("port"), function () {
+  console.log("Proxy server listening on port " + app.get("port"));
+});
